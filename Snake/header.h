4cb62f01a1f668d5fc3ed1a98a6746be8e2cd80e
chauhan739 <chauhan.aryansingh18@gmail.com>
2022-08-26 17:33:09 +0530

    Added comments in header.h

--- Snake/header.h
+++ Snake/header.h
@@ -28,13 +28,32 @@ class GameUtilities {
   float m_timer, m_delay;
 
  public:
+   /**
+    * Constructor. Implementation present in Init.cpp
+    **/
   GameUtilities();
 
+  /**
+   * Funtion calling all other member functions except constructor
+   * Implementation present in RunAppliction.cpp
+   **/
   void start();
 
+  /**
+   * Function creating playing field.
+   * Implementation present in Gtid.cpp
+   **/
   void generateGrid();
 
+  /**
+   * Function for handling user's event.
+   * Implementation present in EventHandler.cpp
+   **/
   void handleEvent();
 
+  /**
+   * Function for drawing all graphics on the screen
+   * Implementation present in Display.cpp
+   **/
   void draw();
 };
