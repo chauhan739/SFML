786ebb7b7e91565cd29023eb9be3ecd7e706b490
chauhan739 <chauhan.aryansingh18@gmail.com>
2022-08-29 18:11:20 +0530

    Implemented user defined snake current direction

--- Snake/EventHandler.cpp
+++ Snake/EventHandler.cpp
@@ -5,4 +5,10 @@ void GameUtilities::handleEvent() {
   while(m_window.pollEvent(event)) {
     if(event.key.code == sf::Keyboard::Escape || event.type == sf::Event::Closed) m_window.close();
   }
+
+  // Handling snake current movement direction
+  if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) m_currDirection = Direction::LEFT;
+  if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) m_currDirection = Direction::RIGHT;
+  if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) m_currDirection = Direction::UP;
+  if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) m_currDirection = Direction::DOWN;
 }
