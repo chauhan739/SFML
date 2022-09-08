77de3a9e2e98613cc6655df1f2a2b7dee85a67bb
chauhan739 <chauhan.aryansingh18@gmail.com>
2022-09-08 23:06:41 +0530

    Added pausing functionality

--- Snake/EventHandler.cpp
+++ Snake/EventHandler.cpp
@@ -6,13 +6,22 @@ void GameUtilities::handleEvent() {
     if(event.key.code == sf::Keyboard::Escape || event.type == sf::Event::Closed) m_window.close();
   }
 
-  // Handling snake current movement direction
-  if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && m_currDirection != Direction::RIGHT)
-    m_currDirection = Direction::LEFT;
-  if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && m_currDirection != Direction::LEFT)
-    m_currDirection = Direction::RIGHT;
-  if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && m_currDirection != Direction::DOWN)
-    m_currDirection = Direction::UP;
-  if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && m_currDirection != Direction::UP)
-    m_currDirection = Direction::DOWN;
+  if(sf::Keyboard::isKeyPressed(sf::Keyboard::Enter) && !m_playingGame)
+    m_playingGame = true;
+
+  if(m_playingGame) {
+    // Handling snake current movement direction
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && m_currDirection != Direction::RIGHT)
+      m_currDirection = Direction::LEFT;
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && m_currDirection != Direction::LEFT)
+      m_currDirection = Direction::RIGHT;
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && m_currDirection != Direction::DOWN)
+      m_currDirection = Direction::UP;
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && m_currDirection != Direction::UP)
+      m_currDirection = Direction::DOWN;
+  }
+
+  // Pausing the game
+  if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && m_playingGame)
+    m_playingGame = false;
 }
--- Snake/SnakeMovement.cpp
+++ Snake/SnakeMovement.cpp
@@ -3,24 +3,26 @@
 void GameUtilities::move() {
   srand((unsigned)time(0));
   
-  // m_snake.at(0) is head of the snake
-  Block segment = m_snake.at(0);
-  
-  if(m_currDirection == Direction::RIGHT) segment.x++;
-  if(m_currDirection == Direction::DOWN) segment.y++;
-  if(m_currDirection == Direction::LEFT) segment.x--;
-  if(m_currDirection == Direction::UP) segment.y--;
+  // m_snake.front() is head of the snake
+  Block segment = m_snake.front();
 
-  if(segment.x == m_fruit.x && segment.y == m_fruit.y) {
-    m_eatingFruit.play();
+  if(m_playingGame) {
+    if(m_currDirection == Direction::RIGHT) segment.x++;
+    if(m_currDirection == Direction::DOWN) segment.y++;
+    if(m_currDirection == Direction::LEFT) segment.x--;
+    if(m_currDirection == Direction::UP) segment.y--;
+
+    if(segment.x == m_fruit.x && segment.y == m_fruit.y) {
+      m_eatingFruit.play();
     
-    m_fruit.x = rand() % 40;
-    m_fruit.y = rand() % 30;
+      m_fruit.x = rand() % 40;
+      m_fruit.y = rand() % 30;
     
-    m_snake.push_front(segment);
-  }
-  else {
-    m_snake.push_front(segment);
-    m_snake.pop_back();
+      m_snake.push_front(segment);
+    }
+    else {
+      m_snake.push_front(segment);
+      m_snake.pop_back();
+    }
   }
 }
